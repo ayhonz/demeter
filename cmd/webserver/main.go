ec6e670036387627c4b0d4a3dfa3006e4cd5df85
ayhonz <janmachacek97@gmail.com>
2024-03-28 22:26:36 +0100

    add echo middleware for logging, templ basic render

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"racook/views"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	ctx.Response().Writer.WriteHeader(statusCode)
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
 
 func main() {
 	e := echo.New()
+	e.Use()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}, took=${latency_human}, error=${error}\n",
+	}))
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "Hello, World!")
+		return Render(c, 200, views.Hello("Suppaaaa"))
 	})
 
-	e.Logger.Fatal(e.Start(":42068"))
+	e.Logger.Fatal(e.Start(":6969"))
 }
